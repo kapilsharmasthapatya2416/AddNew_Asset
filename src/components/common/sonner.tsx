6f04383b86c00f5bf63d0a4d4f9c4d81ee2ff1a0
Vidyanand Sawai <it.DHAWALDEOLASI@sthapatya.in>
2026-01-28 11:12:56 +0530

    Added common components - Accordion, Sonner, Select

---
+++ src/components/common/sonner.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+export interface ToasterPropsExtended extends ToasterProps {
+  className?: string;
+  style?: React.CSSProperties;
+  "data-testid"?: string;
+}
+
+export function Toaster({ className = "", style = {}, ...props }: ToasterPropsExtended) {
+  return (
+    <Sonner
+      className={`toaster group ${className}`}
+      style={style}
+      data-testid={props["data-testid"] || "toaster"}
+      {...props}
+    />
+  );
+}
