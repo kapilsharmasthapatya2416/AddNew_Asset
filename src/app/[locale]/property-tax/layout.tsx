c5836f9a04e431b7ae4103e9b3c7387c2b45aaf0
Mamta-Patil-git <it.mamtapatil@sthapatya.in>
2026-04-22 14:39:43 +0530

    remove this file layout

--- src/app/[locale]/property-tax/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { MainLayout } from '@/components/layout';
-
-interface PropertyTaxLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function PropertyTaxLayout({ children }: Readonly<PropertyTaxLayoutProps>) {
-  return <MainLayout>{children}</MainLayout>;
-}
